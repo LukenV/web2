e04f0e183eb1bf44d3188aa4421ece43cee391d5
Alexis Arnaud - Vinci <128258405+LukenV@users.noreply.github.com>
2023-09-22 14:13:20 +0200

    Deleting 1.2 and merge 1.1 and 1.2 together

--- Exercices/exercice 1.1/myMovies/app.js
+++ Exercices/exercice 1.1/myMovies/app.js
@@ -9,6 +9,38 @@ var filmsRouter = require('./routes/films');
 
 var app = express();
 
+const requests = {};
+
+app.use( (req, res, next) => {
+
+    const requestInfos = req.method + " " + req.path;
+
+    if ( requests[ requestInfos ] === undefined ) {
+
+        requests [ requestInfos ] = 1;
+
+    } else {
+
+        requests [ requestInfos ] ++;
+
+    }
+
+    let string = "Request counter :\n";
+
+    const requestEntries = Object.entries( requests );
+
+    for ( let i=0; i<requestEntries.length; i++ ) {
+
+        string += " -\t" + requestEntries[i][0] + " : " + requestEntries[i][1] + "\n";
+
+    }
+
+    console.log( string );
+
+    next();
+
+});
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
