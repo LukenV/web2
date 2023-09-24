0296552c209ed1d508455acc43f2c727c8ac0af9
LukenV <alexis.arnaud@student.vinci.be>
2023-09-24 21:04:12 +0200

    Edit in "myPizzas" project

--- Tutorials/mesPizzas/app.js
+++ Tutorials/mesPizzas/app.js
@@ -1,13 +1,11 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var pizzasRouter = require('./routes/pizzas');
+const pizzasRouter = require('./routes/pizzas');
 
-var app = express();
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -15,8 +13,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/pizzas', pizzasRouter);
 
 module.exports = app;
