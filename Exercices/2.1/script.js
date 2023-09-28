3f455cd8f7acc288c41383f1a7ce4aae9a8f7386
= <=>
2023-09-28 16:45:11 +0200

    2.1 : HMI : first external script

---
+++ Exercices/2.1/script.js
@@ -0,0 +1,15 @@
+const addDateTime = ( message ) => {
+
+    const dateTime = new Date();
+    const dateString = dateTime.toLocaleDateString();
+    const timeString = dateTime.toLocaleTimeString();
+
+    return `${dateString} ${timeString} ${message}`;
+
+};
+
+const string = addDateTime( "Bienvenue sur cette page web d'exercices !" );
+
+console.log( string );
+
+alert( string );
