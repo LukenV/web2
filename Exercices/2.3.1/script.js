fcf63a4001e3d9023cb5e8f3aab7f109cfd446f0
= <=>
2023-09-29 15:00:55 +0200

    2.3.1 : event object & form

---
+++ Exercices/2.3.1/script.js
@@ -0,0 +1,32 @@
+const form = document.querySelector('form#form');
+const text = document.querySelector('p#text');
+const wish = document.querySelector('input#wish');
+const displayButton = document.querySelector('button#reDisplay');
+
+form.addEventListener('submit', (e) => {
+
+    console.log( "Form submitted" );
+
+    form.style.display = 'none';
+
+    text.innerHTML = `<p> ${wish.value} </p>`;
+
+    wish.value = '';
+
+    displayButton.style.display = 'block';
+
+    e.preventDefault();
+
+});
+
+displayButton.addEventListener('click', (e) => {
+
+    console.log( "button" );
+
+    displayButton.style.display = 'none';
+
+    text.innerHTML = '';
+
+    form.style.display = 'block';
+
+});
